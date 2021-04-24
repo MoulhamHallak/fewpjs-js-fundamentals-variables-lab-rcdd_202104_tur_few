37b961d86e1f97ad816ecf752e0d64518f393017
Moulham Hallak <mou990@gmail.com>
2021-04-24 10:01:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
 // companyName = 'Chelsea';
+<<<<<<< HEAD
 // companyName = "Susan Smith";
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = "Susan Smith";
+=======
+companyName = "Susan Smith";
+>>>>>>> 4f3f54155f58bef855bd4d882bb9364d905d0b32
